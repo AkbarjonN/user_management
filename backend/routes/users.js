67c3389d8bcffd059563725002879b76a2916d01
Akbar <n.akbarjon05@gmail.com>
2025-07-31 17:48:29 +0300

    add smth

--- backend/routes/users.js
+++ backend/routes/users.js
@@ -1,24 +1,26 @@
 import express from "express";
 import { User } from "../models/User.js";
+import { authMiddleware } from "../middleware/auth.js"; 
 
 const router = express.Router();
-router.get("/", async (req, res) => {
+router.get("/", authMiddleware, async (req, res) => {
   const users = await User.findAll();
   res.json(users);
 });
 
-router.post("/block", async (req, res) => {
+router.post("/block", authMiddleware, async (req, res) => {
   const { ids } = req.body;
   await User.update({ blocked: true }, { where: { id: ids } });
   res.json({ message: "Blocked successfully" });
 });
 
-router.post("/unblock", async (req, res) => {
+router.post("/unblock", authMiddleware, async (req, res) => {
   const { ids } = req.body;
   await User.update({ blocked: false }, { where: { id: ids } });
   res.json({ message: "Unblocked successfully" });
 });
-router.post("/delete", async (req, res) => {
+
+router.post("/delete", authMiddleware, async (req, res) => {
   const { ids } = req.body;
   await User.destroy({ where: { id: ids } });
   res.json({ message: "Selected users deleted" });
