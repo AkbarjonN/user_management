a79106156570f40abadc75111c3be00efa66f00e
Akbar <n.akbarjon05@gmail.com>
2025-07-31 19:12:52 +0300

    front

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -11,6 +11,7 @@ function App() {
       <Routes>
         <Route path="/login" element={<Login />} />
         <Route path="/register" element={<Register />} />
+        <Route path="/" element={<Register />} />   
         <Route path="/users" element={localStorage.getItem("token") ? <Users /> : <Navigate to="/login" />}/>
       </Routes>
     </BrowserRouter>
